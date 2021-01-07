e3faa28b7fd3a37fd7c94dbe48f977cae7b3962c
Sam King <kingst@gmail.com>
2021-01-07 12:03:41 -0800

    End of lecture

---
+++ EmojiChat/EmojiChat/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  EmojiChat
+//
+//  Created by Sam King on 1/7/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ EmojiChat/EmojiChat/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  EmojiChat
+//
+//  Created by Sam King on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func smilyFacePress() {
+        print("smily face pressed")
+    }
+    
+}
+
