5adc88ca5a4d91dd3836810fc92ffa14dedab5f7
Sam King <kingst@gmail.com>
2021-02-18 11:16:12 -0800

    End of lecture

--- CardScanThreads/CardScanThreads/ViewController.swift
+++ CardScanThreads/CardScanThreads/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ddOcrResult: UILabel!
     @IBOutlet weak var finalOcrResult: UILabel!
     
+    let mlQueue = DispatchQueue(label: "ml queue")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,18 +31,57 @@ class ViewController: UIViewController {
     
     func ocrOn(image: UIImage) -> String {
         guard let croppedImage = crop(image: image) else { return "Error" }
-        self.imageView.image = UIImage(cgImage: croppedImage)
+        DispatchQueue.main.async {
+            self.imageView.image = UIImage(cgImage: croppedImage)
+        }
         
-        return "Not found"
+        var appleOcrPrediction: CreditCardOcrPrediction?
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        DispatchQueue.global(qos: .userInitiated).async {
+            let appleOcr = CardScan.AppleCreditCardOcr(dispatchQueueLabel: "apple ocr")
+            let result = appleOcr.recognizeCard(in: croppedImage, roiRectangle: CGRect(x: 0, y: 0, width: croppedImage.width, height: croppedImage.height))
+            appleOcrPrediction = result
+            semaphore.signal()
+        }
+        
+        var ddOcrPrediction: CreditCardOcrPrediction?
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let ddOcr = CardScan.SSDCreditCardOcr(dispatchQueueLabel: "dd ocr")
+            let result = ddOcr.recognizeCard(in: croppedImage, roiRectangle: CGRect(x: 0, y: 0, width: croppedImage.width, height: croppedImage.height))
+            ddOcrPrediction = result
+            semaphore.signal()
+        }
+        
+        semaphore.wait()
+        semaphore.wait()
+        
+        DispatchQueue.main.async {
+            self.appleOcrResult.text = appleOcrPrediction?.number ?? "Not found"
+            self.ddOcrResult.text = ddOcrPrediction?.number ?? "Not found"
+        }
+            
+        return ddOcrPrediction?.number ?? appleOcrPrediction?.number ?? "Not found"
     }
-        
+    
+    func runInTheBackground(image: UIImage, completion: @escaping ((String) -> Void)) {
+        mlQueue.async {
+            let result = self.ocrOn(image: image)
+            DispatchQueue.main.async { completion(result) }
+        }
+    }
+    
     @IBAction func runOcrPress() {
         let image = #imageLiteral(resourceName: "sams_bofa")
         ddOcrResult.text = nil
         appleOcrResult.text = nil
         finalOcrResult.text = nil
         
-        finalOcrResult.text = ocrOn(image: image)
+        //finalOcrResult.text = ocrOn(image: image)
+        runInTheBackground(image: image) { result in
+            self.finalOcrResult.text = result
+        }
     }
     
 }
